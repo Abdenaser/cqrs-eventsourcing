73724783a3664bca0edf88ffb3816d99eefb1880
Abdenaser <a.aroukhsiss_etu@enset-media.ac.ma>
2022-12-25 21:05:58 +0100

    Initial commit

---
+++ src/main/java/ma/enset/comptecqrses/CompteCqrsEsApplication.java
@@ -0,0 +1,16 @@
+package ma.enset.comptecqrses;
+
+import ma.enset.comptecqrses.commonapi.mappers.AccountMapper;
+import org.mapstruct.factory.Mappers;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CompteCqrsEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompteCqrsEsApplication.class, args);
+    }
+
+}
